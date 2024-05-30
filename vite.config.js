5b15bb1122c455869948ca606a9eea1818b61e64
Uttam Mishra <mishrauttam526@gmail.com>
2024-05-30 11:49:04 +0530

    add:deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gemini-clone/",
   plugins: [react()],
+  base: "/gemini-clone/"
 })
